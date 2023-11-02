6111d639a939ac9c45ee7e60620a1a8a18652a59
Hannah2609 <hann7419@stud.kea.dk>
2023-11-02 10:05:53 +0100

    metaData

--- src/app/henry/page.jsx
+++ src/app/henry/page.jsx
@@ -1,15 +1,22 @@
-export default async function Henry() {
+export async function generateMetadata() {
+  const res = await fetch("https://nice-dogs.vercel.app/api/dogs?slug=henry");
+  const data = await res.json();
+  return {
+    title: data.name,
+    description: `loves ${data.favouriteColor}`,
+  };
+}
 
-   const res = await fetch("https://nice-dogs.vercel.app/api/dogs?slug=henry");
-   const data = await res.json();
-   console.log(data); 
+export default async function Henry() {
+  const res = await fetch("https://nice-dogs.vercel.app/api/dogs?slug=henry");
+  const data = await res.json();
+  console.log(data);
   return (
     <main>
       <h1>{data.name}</h1>
       <h2>{data.slug}</h2>
       <p>my faverite color is {data.favouriteColor}</p>
       <img src={data.image.url} alt="henry pic" />
-      
     </main>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,12 @@
+export const metadata = {
+  title: "home",
+  description: "this is my first next.js project",
+};
+
 export default function Home() {
   return (
-  <main>
-    <h1>Hej</h1>
-  </main>
+    <main>
+      <h1>Hej</h1>
+    </main>
   );
 }
