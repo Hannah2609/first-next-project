ffe4b2252bb3f35318fabb814d7171fe4cae5ce1
Hannah2609 <hann7419@stud.kea.dk>
2023-11-02 09:53:53 +0100

    henry

---
+++ src/app/henry/page.jsx
@@ -0,0 +1,15 @@
+export default async function Henry() {
+
+   const res = await fetch("https://nice-dogs.vercel.app/api/dogs?slug=henry");
+   const data = await res.json();
+   console.log(data); 
+  return (
+    <main>
+      <h1>{data.name}</h1>
+      <h2>{data.slug}</h2>
+      <p>my faverite color is {data.favouriteColor}</p>
+      <img src={data.image.url} alt="henry pic" />
+      
+    </main>
+  );
+}
